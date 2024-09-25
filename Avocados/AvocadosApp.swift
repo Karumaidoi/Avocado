4fec49986463e9a084eba8267aacbc4d4e59d3d4
Karumaidoi <mainahmwangi12@gmail.com>
2024-09-25 17:52:21 +0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Alex Maina on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
